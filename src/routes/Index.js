920eee93027cd50409c242bcc661810b97481c77
VaasRaider <esli1234567@gmail.com>
2020-11-26 10:16:23 -0600

    Update Index.js

--- src/routes/Index.js
+++ src/routes/Index.js
@@ -9,22 +9,20 @@ router.get ('/', async (req, res)=>{
     res.render('Index', {notes});
 });
 
-router.get ('/test', async (req, res)=>{
+router.get ('/test_get', async (req, res)=>{
     //res.sendFile(path.join(__dirname, 'views/Index.html'));
     const notes = await Note.find().lean();
     res.json(notes);
     //res.render('Index', {notes});
 });
 
-/*
-router.post('/', (req, res) => {
+
+router.post('/test_post', (req, res) => {
     const{title, description, user} = req.body;
      const newNote = new Note({title, description, user});
      await newNote.save();
-    console.log('Got body:', req.body);
-    res.sendStatus(200);
 });
-*/
+
 router.get ('/about', (req, res)=>{
     //res.sendFile(path.join(__dirname, 'views/Index.html'));
     res.render('about' );
